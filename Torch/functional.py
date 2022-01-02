6e5e998fe5152ba90a793f2f583f3ef4c92992ff
PhilipNaveen <64291543+PhilipNaveen@users.noreply.github.com>
2022-01-02 10:24:34 -0500

    Create functional.py

---
+++ Torch/functional.py
@@ -0,0 +1,14 @@
+'''
+Phish activation functional
+'''
+
+#importing pytorch
+import torch
+import torch.nn.functional as F
+
+@torch.jit.script
+def phish(input):
+    '''
+    Applies the Phish activation function of f(x) = xTanH(GELU(x))
+    '''
+    return input * torch.tanh(F.gelu(input))
