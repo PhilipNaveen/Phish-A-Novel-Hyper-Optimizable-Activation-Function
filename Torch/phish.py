c5cb4accb90e07ab977cbf3079d9e62459f00466
PhilipNaveen <64291543+PhilipNaveen@users.noreply.github.com>
2022-01-02 10:29:54 -0500

    Create phish.py

---
+++ Torch/phish.py
@@ -0,0 +1,19 @@
+#applies the Phish activation function: f(x) = xTanH(GELU(x))
+
+#pytorch dependency
+from torch import nn
+
+#import activation functions
+import Phish.Torch.functional as Func
+
+class Phish(nn.Module):
+    #input shape is (N, *) where * denotes the number of additional dimensions
+    #output shape is the same as the input
+ 
+    #initialization
+    def __init__(self):
+        super().__init__()
+
+    #forward pass through the activation function
+    def forward(self, input):
+        return Func.mish(input)
